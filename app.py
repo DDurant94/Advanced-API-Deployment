f20fa64812c99ec71c4d25e248519d8163232f87
DDurant94 <dannyjdurant@gmail.com>
2024-10-06 17:35:16 -0500

    fix

--- app.py
+++ app.py
@@ -39,8 +39,6 @@ def create_app(config_name):
   app = Flask(__name__)
   app.config['SQLALCHEMY_DATABASE_URI'] = DC.SQLALCHEMY_DATABASE_URI
   app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = DC.SQLALCHEMY_TRACK_MODIFICATIONS
-  app.config.from_object(f'config.{config_name}')
-
   try:
     db.init_app(app)
     ma.init_app(app)
