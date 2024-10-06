c815dcfc417594468e1ea5f494c985f3898a675e
DDurant94 <dannyjdurant@gmail.com>
2024-10-06 16:44:57 -0500

    URI/MODS fix in config

--- config.py
+++ config.py
@@ -6,8 +6,8 @@ DATABASE_URL = os.getenv('DATABASE_URL')
 
 # connecting uri for postgras database
 class DevelopmentConfig:
-  SQLALCHEMY_DATABASE_URI = DATABASE_URL
-  SQLALCHEMY_TRACK_MODIFICATIONS = False
+  ['SQLALCHEMY_DATABASE_URI'] = DATABASE_URL
+  ['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
   CACHE_TYPE = 'SimpleCache'
   
   
