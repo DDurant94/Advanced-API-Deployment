d15c1360e89201aa553bcc119103b759cb371de5
DDurant94 <dannyjdurant@gmail.com>
2024-10-06 21:26:59 -0500

    yaml dep changes

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ DATABASE_URL = os.getenv('DATABASE_URL')
 
 # connecting uri for postgras database
 class DevelopmentConfig:
-  SQLALCHEMY_DATABASE_URI = 'postgresql://e_commerce_api_r2mn_user:pddIAMcxJxjyDeYsH5YV3b9q5nDfxaUP@dpg-cs1f00dds78s73b630sg-a.oregon-postgres.render.com/e_commerce_api_r2mn'
+  SQLALCHEMY_DATABASE_URI = DATABASE_URL
   SQLALCHEMY_TRACK_MODIFICATIONS = False
   CACHE_TYPE = 'SimpleCache'
   
